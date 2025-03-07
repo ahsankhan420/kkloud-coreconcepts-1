44981c654b8c54d8d54ce8679b56ac7a0cf7308f
ahsankhan420 <ahsanullahkhan881@gmail.com>
2025-03-07 15:53:08 +0500

    added permission

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-echo "Hello, I am a script to run cowsay and grep"
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am DRAGON.....RAWR" >> dragon.txt
 grep -i "DRAGON" dragon.txt
